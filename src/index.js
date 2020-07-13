871b7156ec911f1636eddbd5869bb53c916b67eb
Thomas Clark <thomas.w.clark.22@darmtouth.edu>
2020-07-13 15:22:03 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+setInterval(() => {
+  $('#main').html(`You have been on this page for ${num} seconds`);
+  num += 1;
+},
+1000);
